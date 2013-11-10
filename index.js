cb80d9974d434c789aab78f919d43114754309f5
Janessa Det <jandet@gmail.com>
2013-11-09 21:05:27 -0500

    make the position the control target instead of camera target

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ exports.PositionAudio.prototype.load = function(callback) {
 function tick() {
 
 	game.camera.updateMatrixWorld();
-	var position = new game.THREE.Vector3().getPositionFromMatrix(game.camera.matrixWorld);
+	var position = game.controls.target().avatar.position;
 	var velocity = game.controls.target().velocity.clone();
 
 	audioContext.listener.setPosition(position.x, position.y, position.z);
